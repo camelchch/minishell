dff6462b5fa6fff6efab03270c442c227a2611e5
Saiqiong XIAO <saxiao@e1r5p21.42.fr>
2018-02-01 16:36:16 +0100

    update

--- sh_table.c
+++ sh_table.c
@@ -6,11 +6,12 @@
 /*   By: saxiao <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/31 15:43:03 by saxiao            #+#    #+#             */
-/*   Updated: 2018/01/31 17:28:03 by saxiao           ###   ########.fr       */
+/*   Updated: 2018/02/01 14:54:19 by saxiao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <dirent.h>
+#include <stdlib.h>
 #include <errno.h>
 #include "minishell.h"
 
@@ -28,18 +29,41 @@ char	**path(char **env)
 	return (ft_split(*env + 5, ":"));
 }
 
+int		calcu_index(t_table *add)
+{
+	int		cal;
+	int		i;
+
+	cal = 0;
+	i = 0;
+	while (add->name[i])
+		cal = add->name[i++] + cal;
+	cal = cal % 100;
+	return (cal);
+}
+
+void	addin_table(:wq
+
 void	init_shtable(t_sh *table, char **path)
 {
 	DIR				*dirp;
 	struct dirent	*dir;
 	int				index;
+	t_table			*add;
 
 	while (*path)
 	{
 		if (!(dirp = opendir(*path)))
-			perror("open directory failed");
-		while (dir = readdir(dirp))
+			perror("open path failed");
+		while ((dir = readdir(dirp)))
 		{
+			add = (t_table *)malloc(sizeof(t_table));
+			ft_strcpy(add->name, dir->d_name);
+			ft_strcpy(add->path, *path);
+			ft_strcat(add->path, "/");
+			ft_strcat(add->path, add->name);
+			add->next = NULL;
+			index = calcu_index(add);
 
 		}
 
